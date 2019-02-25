d4a421d133de1ad7703cf6bbe276cca645738551
tony2037 <zyex030640417@gmail.com>
2019-02-25 14:41:31 +0800

    Fisrt verions experient

--- rdtsc/rdtsc.c
+++ rdtsc/rdtsc.c
@@ -6,10 +6,6 @@ int64_t cpucycle (void) {
     unsigned int hi, lo;
 
     __asm__ volatile("rdtsc\n\t" : "=a"(lo), "=d"(hi));
-    int64_t result = ((int64_t) lo | (((int64_t) hi) << 32));
-    return result;
-}
-
-int main (int argc, char **argv) {
-    cpucycle();
+    // int64_t result = ((int64_t) lo | (((int64_t) hi) << 32));
+    return ((int64_t) lo | (((int64_t) hi) << 32));;
 }
